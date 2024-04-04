42ac8bcc2650eb0a139eec30349383cd91c1bb56
dzyrick2 <daniel.gayao7@gmail.com>
2024-04-04 15:38:13 +0800

    fixed route for iput case sensitivity

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -4,42 +4,36 @@ import { NextApiRequest } from "next";
 
 const prisma = new PrismaClient();
 export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const location = searchParams ? searchParams.get("location") : null;
+  const destination = searchParams ? searchParams.get("destination") : null;
+
+  const locationValue = location !== null ? location : "";
+  const destinationValue = destination !== null ? destination : "";
   try {
     const post = await prisma.schedule.findMany({
-      //   where: {
-      //     route: {
-      //       some: {
-      //         location: { contains: "", mode: "insensitive" },
-      //       },
-      //     },
-      //   },
+      where: {
+        route: {
+          some: {
+            location: { contains: `${locationValue}`, mode: "insensitive" },
+            destination: {
+              contains: `${destinationValue}`,
+              mode: "insensitive",
+            },
+          },
+        },
+      },
       include: {
         route: true,
         bus: true,
       },
     });
-    const searchParams = req.nextUrl.searchParams;
-    const location = searchParams ? searchParams.get("location") : null;
-    const destination = searchParams ? searchParams.get("destination") : null;
-    // if (location !== "string") {
-    //   throw new Error("failed to request data");
-    // }
 
-    const filteredQuery =
-      location && destination
-        ? post.filter(
-            (data) =>
-              data.route[0].location.includes(location) &&
-              data.route[0].destination.includes(destination)
-          )
-        : post;
-    return NextResponse.json(filteredQuery, { status: 200 });
+    return NextResponse.json(post, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "could not delete post" },
       { status: 500 }
     );
   }
-
-  //   return NextResponse.json({ message: "hey" }, { status: 200 });
 }
