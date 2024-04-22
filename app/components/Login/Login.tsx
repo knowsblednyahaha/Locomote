c3da837ea85b863a19de0e588c24d9eab9c89f5a
dzyrick2 <daniel.gayao7@gmail.com>
2024-04-22 08:59:22 +0800

    added the ui of login

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -2,19 +2,36 @@ import React from "react";
 
 export const Login = () => {
   return (
-    <section className="max-w-[1440px] w-full w-full flex justify-between items-center py-5 px-5 md:px-10 lg:px-20 z-20">
-      <div>
-        <div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <div>
-            <button>Login</button>
-          </div>
+    <section className="max-w-[1440px] w-full h-[90vh] flex justify-center items-center py-5 px-5 md:px-10 lg:px-20 z-20 m-auto ">
+      <div className="w-2/6 p-10 border-gray-400 shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-2xl">
+        <div className="text-center pb-10">
+          <h3 className="text-2xl font-semibold">Sign In</h3>
         </div>
+        <form className="flex flex-col gap-y-5">
+          <div className="flex flex-col w-full">
+            <label htmlFor="email">Email or Username</label>
+            <input
+              type="text"
+              className="border-2 h-[40px] rounded-2xl pl-3"
+              id="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              className="border-2 h-[40px] rounded-2xl pl-3"
+              id="password"
+              placeholder="Your password"
+            />
+          </div>
+          <div className="text-center">
+            <button className="w-[100px] bg-black text-white p-3 rounded-2xl">
+              Sign In
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
