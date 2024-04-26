3d3a9c4d0f185a3b51a7f2c2fa9843aa4eff8ad1
dzyrick2 <daniel.gayao7@gmail.com>
2024-04-26 21:23:26 +0800

    fixed the contact page ui

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -3,30 +3,49 @@ import Image from "next/image";
 
 const Contact = () => {
   return (
-    <section className="max-w-[1440px] w-full w-full py-10 px-5 md:px-10 lg:px-20 z-20 m-auto">
-      <div className="w-full">
-        <div className="w-4/6">
-          <div className="w-1/3">
+    <section className="max-w-[1440px] h-[51.1vh] w-full w-full py-10 px-5 md:px-10 lg:px-20 z-20 m-auto">
+      <div className="w-full flex">
+        <div className="w-1/3">
+          <div className="mb-5">
             <h3 className="text-3xl">Contact Us</h3>
           </div>
-          <div className="w-1/3">
+          <div>
             <h3 className="text-xl">Are you in need of assistance?</h3>
           </div>
-          <form action="">
-            <div>
-              <input type="text" className="border" />
-            </div>
-            <div>
-              <input type="text" className="border" />
-            </div>
-            <div>
-              <button>Submit</button>
-            </div>
-          </form>
         </div>
-        <div className="w-2/6 relative">
-          {/* <Image src={"/"} alt={""} /> */}
-        </div>
+        <form action="" className="w-2/3">
+          <div className="flex gap-x-10 pb-5">
+            <div className="flex flex-col w-1/2 gap-y-2">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="border border-black rounded-xl h-[50px] pl-3"
+                id="name"
+              />
+            </div>
+            <div className="flex flex-col w-1/2 gap-y-2">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="border border-black rounded-xl h-[50px] pl-3"
+                id="email"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-2 pb-5">
+            <label htmlFor="message">Message</label>
+            <textarea
+              className="border border-black rounded-xl pl-3 pt-2"
+              id="message"
+              rows={4}
+            />
+          </div>
+          <div className="text-center">
+            <button className="w-[100px] h-[50px] bg-[#FE2F2F] text-white rounded-2xl">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Contact from "../components/contact/Contact";
+import Footer from "../components/Home/Footer";
 
 export default function ContactPage() {
   return (
     <>
       <Header />;
       <Contact />
+      <Footer />
     </>
   );
 }
